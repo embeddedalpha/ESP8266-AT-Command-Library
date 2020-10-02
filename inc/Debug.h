0d80ce53c511c699765b04de9cfa4198ec83e930
Axion Engineering <kunalsalvi63@gmail.com>
2020-10-02 15:21:37 +0530

    Update Debug.h

--- inc/Debug.h
+++ inc/Debug.h
@@ -8,14 +8,15 @@
 #include "USART.h"
 #include "stdio.h"
 
+extern char response[100];
 
 #define DEBUG_UART  USART1
 #define delay				for(int i = 0; i<= 5000000; i++)
 
 void InitConsole(void);
-static void printConsole(char *msg, ...);
+ void printConsole(char *msg, ...);
 int Console_Dump(const char *s);
-char * Console_Return(void);
+char * Console_Return(char *s);
 
 
 #endif
